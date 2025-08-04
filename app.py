505832dd1d545bfe16f06bd9c4d8759bc7bb5651
anshmaheshwari01 <167005656+anshmaheshwari01@users.noreply.github.com>
2025-08-04 17:59:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 import streamlit as st
-from final_backend import answer_question()
+from final_backend import answer_question
 st.set_page_config(page_title="📘 PDF Q&A", layout="centered")
 st.title("📘 Ask Questions from PDF")
 st.markdown("PDF Used: *Nonfiction Reading Test Black Friday.pdf*")
